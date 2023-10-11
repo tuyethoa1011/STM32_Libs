9eba9d02c21d9448dcc6cdef9128e68c0423493b
Ngô Lê Tuyết Hoa <80042406+tuyethoa1011@users.noreply.github.com>
2023-10-11 13:41:50 +0700

    Add files via upload

---
+++ buttons_lib/button.c
@@ -0,0 +1,97 @@
+/**
+  ******************************************************************************
+  * File: button.c
+  * Author: Ngo Le Tuyet Hoa - Sam
+  * Author's contact: ngltuyethoa1011@gmail.com
+  * Last Modification: 11/10/2023
+  * Used for: STM32F103C8T6 - Could be used for other STM32 KIT/ MCU but needs modification.
+  * Lib name: Button
+  * @All rights reserved. Please use for learning and developing purpose
+  ******************************************************************************
+*/
+#include "button.h"
+
+__weak void button_pressing_callback(Button_Typedef *ButtonX)
+{
+
+}
+
+__weak void button_release_callback(Button_Typedef *ButtonX)
+{
+
+}
+
+__weak void button_shortpressing_callback_500ms(Button_Typedef *ButtonX)
+{
+
+}
+
+__weak void button_longpressing_callback_500ms(Button_Typedef *ButtonX)
+{
+
+}
+
+////them cac ham con lai lien quan den xu ly nut bam vao
+////>= 500 ms < 500ms
+
+__weak void button_handle(Button_Typedef *ButtonX)
+{
+	uint8_t state =  HAL_GPIO_ReadPin(ButtonX->GPIOx, ButtonX->GPIO_Pin); //get button state
+	//---- Xu ly loc nhieu ----
+	if(state != ButtonX->button_filter)
+	{
+		ButtonX->button_filter = state;
+		ButtonX->is_debouncing = 1;
+		ButtonX->time_debounce = HAL_GetTick();
+	}
+	//---- Xac lap tin hieu ----
+	if(ButtonX->is_debouncing && (HAL_GetTick() - ButtonX->time_debounce >= 15))
+	{
+		ButtonX->button_current = ButtonX->button_filter;
+		ButtonX->is_debouncing = 0;
+	}
+	//---- Xu ly tin hieu nut nhan ----
+	if(ButtonX->button_current != ButtonX->button_last) //phat hien tin hieu nut nhat chinh xac sau khi xu ly thay doi
+	{
+		//nut bam nhan xuong
+		if(ButtonX->button_current == 0)
+		{
+			//button_pressing_callback(ButtonX);
+			ButtonX->time_btn_press = HAL_GetTick();
+			ButtonX->is_press_timeout = 1;
+		}
+		else
+		{
+			if(HAL_GetTick() - ButtonX->time_btn_press < 500) //xu ly nhan nha nhanh < 500ms
+			{
+				button_shortpressing_callback_500ms(ButtonX);
+			}
+			ButtonX->is_press_timeout = 0 ;
+			ButtonX->is_long_press = 0 ;
+			long_press_detect = ButtonX->is_long_press;
+			//button_release_callback(ButtonX);
+		}
+			ButtonX->button_last = ButtonX->button_current;
+	}
+
+	//-------------Xu li nhan giu lau hon 500ms----------------
+	if(ButtonX->is_press_timeout == 1 && (HAL_GetTick() - ButtonX->time_btn_press >= 500)){
+		ButtonX->is_long_press = 1;
+		button_longpressing_callback_500ms(ButtonX);
+		ButtonX->is_press_timeout = 0;
+
+	}
+}
+
+//tong cong co bao nhieu ham callback: press_callback, release callback, longpressing, shortpressing
+
+
+void button_Init(Button_Typedef *ButtonX,GPIO_TypeDef *GPIOx,uint16_t GPIO_Pin)
+{
+	ButtonX->GPIOx = GPIOx;
+	ButtonX->GPIO_Pin = GPIO_Pin;
+
+	ButtonX->button_current = 1;
+	ButtonX->button_last = 1;
+	ButtonX->button_filter = 1;
+}
