d17997ec68859d73775b7e3b63db63f98da22ee5
Ngô Lê Tuyết Hoa <80042406+tuyethoa1011@users.noreply.github.com>
2023-10-21 14:18:00 +0700

    Add files via upload

---
+++ uart_esp_debug_module/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+
+
+#define TXD_PIN (GPIO_NUM_1) //Chân được cấu hình dựa trên KIT ESP được sử dụng (Current: ESP32-S2 AI Thinker)
+#define RXD_PIN (GPIO_NUM_3)
+
+
+static const int RX_BUF_SIZE = 1024;
+
+void uart_debug_init(void) {
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+
+void app_main(void)
+{   
+    uart_debug_init(); //uart init 
+}
